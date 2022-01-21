b39b6a22c2f95b9507642a638e75e49b85c0841a
Tanner Robertson <159tannerrobertson@gmail.com>
2022-01-21 12:36:37 -0700

    edit to allow env to work in package

--- config/connectData.php
+++ config/connectData.php
@@ -1,16 +1,19 @@
 <?php
-$env = include('config/connectData.php');
+
+$dotenv = \Dotenv\Dotenv::create(__DIR__ . '/..');
+$dotenv->load();
+
 return [
     'production' => [
         'url' => 'https://production-uri.com/transactional/journey',
-        'apiKey' => 'unknownkeygoeshere'
+        'apiKey' => getenv('APP_ENV_PRODUCTION') ?: 'APP_ENV_PRODUCTION'
     ],
     'staging' => [
         'url' => 'https://staging-uri.com/transactional/journey',
-        'apiKey' => 'unknownkeygoeshere2'
+        'apiKey' => getenv('APP_ENV_STAGING') ?: 'APP_ENV_STAGING'
     ],
     'development' => [
         'url' => 'https://testing-uri.com/transactional/journey',
-        'apiKey' => 'unknownkeygoeshere3'
+        'apiKey' => getenv('APP_ENV_DEVELOPMENT') ?: 'APP_ENV_DEVELOPMENT'
     ]
 ];
