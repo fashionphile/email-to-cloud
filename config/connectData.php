97db5f821f17bb14f069023f97bbd7026d143fce
Tanner Robertson <159tannerrobertson@gmail.com>
2022-03-17 13:39:27 -0600

    adjust config name and naming for composer

--- config/connectData.php
+++ config/connectData.php
@@ -2,15 +2,15 @@
 
 return [
     'production' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL') ?? 'https://www.fakeurl.test',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY'] ?? 'EMAIL_CLOUD_KEY'
+        'url' => $_ENV('EMAIL_CLOUD_URL_PROD') ?? 'https://www.fakeurl.test',
+        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_PROD'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'staging' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL') ?? 'https://www.fakeurl.test',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY'] ?? 'EMAIL_CLOUD_KEY'
+        'url' => $_ENV('EMAIL_CLOUD_URL_STAGING') ?? 'https://www.fakeurl.test',
+        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_STAGING'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'development' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL') ?? 'https://www.fakeurl.test',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY'] ?? 'EMAIL_CLOUD_KEY'
+        'url' => $_ENV('EMAIL_CLOUD_URL_DEV') ?? 'https://www.fakeurl.test',
+        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_DEV'] ?? 'EMAIL_CLOUD_KEY'
     ]
 ];
