6265a28f7d5b67fb9afb65ca71b67d7d53545ca5
Tanner Robertson <159tannerrobertson@gmail.com>
2022-03-22 16:56:06 -0600

    adjust config for fewer requirements

--- config/connectData.php
+++ config/connectData.php
@@ -2,15 +2,15 @@
 
 return [
     'production' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL_PROD') ?? 'https://www.fakeurl.test',
+        'url' => $_ENV('EMAIL_CLOUD_URL_PROD') ?? 'https://production-apigateway.fashionphile.com/transactional/journey',
         'apiKey' => $_ENV['EMAIL_CLOUD_KEY_PROD'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'staging' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL_STAGING') ?? 'https://www.fakeurl.test',
+        'url' => $_ENV('EMAIL_CLOUD_URL_STAGING') ?? 'https://staging-apigateway.fashionphile.com/transactional/journey',
         'apiKey' => $_ENV['EMAIL_CLOUD_KEY_STAGING'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'development' => [
-        'url' => $_ENV('EMAIL_CLOUD_URL_DEV') ?? 'https://www.fakeurl.test',
+        'url' => $_ENV('EMAIL_CLOUD_URL_DEV') ?? 'https://testing-apigateway.fashionphile.com/transactional/journey',
         'apiKey' => $_ENV['EMAIL_CLOUD_KEY_DEV'] ?? 'EMAIL_CLOUD_KEY'
     ]
 ];
