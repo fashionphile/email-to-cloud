10a0fb0d382c6ae9effea00c407a661f4d6dffe1
Tanner Robertson <159tannerrobertson@gmail.com>
2022-03-23 17:02:09 -0600

    adjust env names to match

--- config/connectData.php
+++ config/connectData.php
@@ -3,14 +3,14 @@
 return [
     'production' => [
         'url' => $_ENV['EMAIL_CLOUD_URL_PROD'] ?? 'https://production-apigateway.fashionphile.com/transactional/journey',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_PROD'] ?? 'EMAIL_CLOUD_KEY'
+        'apiKey' => $_ENV['APP_ENV_PRODUCTION'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'staging' => [
         'url' => $_ENV['EMAIL_CLOUD_URL_STAGING'] ?? 'https://staging-apigateway.fashionphile.com/transactional/journey',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_STAGING'] ?? 'EMAIL_CLOUD_KEY'
+        'apiKey' => $_ENV['APP_ENV_STAGING'] ?? 'EMAIL_CLOUD_KEY'
     ],
     'development' => [
         'url' => $_ENV['EMAIL_CLOUD_URL_DEV'] ?? 'https://testing-apigateway.fashionphile.com/transactional/journey',
-        'apiKey' => $_ENV['EMAIL_CLOUD_KEY_DEV'] ?? 'EMAIL_CLOUD_KEY'
+        'apiKey' => $_ENV['APP_ENV_DEVELOPMENT'] ?? 'EMAIL_CLOUD_KEY'
     ]
 ];
