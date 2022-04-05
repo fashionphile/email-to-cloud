c2576e46dda86fb7af12ad46541cf430402ac91b
Tanner Robertson <159tannerrobertson@gmail.com>
2022-04-05 15:58:35 -0600

    testing new package type

--- src/SendEmail.php
+++ src/SendEmail.php
@@ -1,159 +1,85 @@
 <?php
 
-namespace FPemail;
+namespace App\Jobs\Mail\SalesForceMarketingCloud;
 
-use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
 
-class SendEmail
+class EmailBuilder
 {
-    public $config;
     public $data;
+    public $url;
+    public $headers;
 
-    /**
-     * @throws Exception
-     */
-    public function __construct(
-        string $definitionKey,
-        string $environment = 'test'
-    ) {
-        $this->data['definitionKey'] = $definitionKey;
-        $this->setConfig($environment);
-    }
-
-    /**
-     * Send request to sfmc lambda triggering email
-     *
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws Exception
-     */
-    public function send(): Response
+    public function sendEmail()
     {
-        if ($message = $this->validate()) {
-            throw new Exception(json_encode($message));
+        if ($message = $this->validate() || $this->shouldNotSend()) {
+            return json_encode($message ?: 'Should not send email');
         }
 
         return $this->postRequest();
     }
 
-    /**
-     * Set the user email and id in data object
-     *
-     * @param  string  $email
-     * @param  int  $userId
-     * @return void
-     */
+    private function postRequest(): Response
+    {
+        $client = new Client();
+
+        return $client->request('POST', $this->url, [
+            'headers' => $this->headers,
+            'body' => json_encode($this->data, true)
+        ]);
+    }
+
+    private function validate(): array
+    {
+        $message = [];
+        $requiredData = [
+            'definitionKey' => 'definitionKey missing, this is a marketing cloud email key.',
+            'email' => 'email missing, user\'s email that is going to get sent.',
+            'user_id' => 'user_id missing, user id missing required for email service.',
+        ];
+
+        foreach ($requiredData as $key => $missingMessage) {
+            if (!array_key_exists($key, $this->getData())) {
+                $message[$key] = $missingMessage;
+            }
+        }
+        if (!$this->url) {
+            $message['url'] = 'Url Required for package to send email';
+        }
+
+        return $message;
+    }
+
     public function to(string $email, int $userId): void
     {
         $this->data['email'] = $email;
         $this->data['user_id'] = $userId;
     }
 
-    /**
-     * Set the email specific data if needed
-     *
-     * @param  array  $emailSpecificData
-     * @return void
-     */
-    public function setData(array $emailSpecificData = []): void
+    public function guzzleRequirement(array $guzzle): void
+    {
+        $this->setHeaders($guzzle['headers']);
+        $this->setUrl($guzzle['url']);
+    }
+
+    public function buildEmailContent(array $emailSpecificData = []): void
     {
         $this->data['data'] = $emailSpecificData;
     }
 
-    /**
-     * Set config based on found or given environment
-     *
-     * @param  string  $environment
-     * @return void
-     */
-    private function setConfig(string $environment): void
+    public function emailDefinitionKey(string $definitionKey)
     {
-        $connectConfig = include(__DIR__.'/../config/connectData.php');
-
-        switch ($environment) {
-            case 'production':
-                $this->config = $connectConfig['production'];
-                break;
-            case 'staging':
-                $this->config = $connectConfig['staging'];
-                break;
-            default:
-                $this->config = $connectConfig['development'];
-        }
-
+        $this->data['definitionKey'] = $definitionKey;
     }
 
-    /**
-     * Validate minimum requirements for eamils and send message if failure
-     *
-     * @return array
-     * @throws Exception
-     */
-    private function validate(): array
+    public function setHeaders(array $headers)
     {
-        $message = [];
-        $required = [
-            'definitionKey' => 'definitionKey missing, this is a marketing cloud email key.',
-            'email' => 'email missing, user\'s email that is going to get sent.',
-            'user_id' => 'user_id missing, user id missing required for email service.',
-        ];
-
-        foreach ($required as $key => $missingMessage) {
-            if (!array_key_exists($key, $this->getData())) {
-                $message[$key] = $missingMessage;
-            }
-        }
-
-        return $message;
+        $this->headers = $headers;
     }
 
-    /**
-     * Send Built request to trigger email
-     *
-     * @return Response
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    private function postRequest(): Response
+    public function setUrl(string $url)
     {
-        $client = new Client();
-
-        return $client->request('POST', $this->config['url'], [
-            'headers' => $this->getHeaders(),
-            'body' => json_encode($this->getData(), true)
-        ]);
-    }
-
-    /**
-     * Set the email specific data if needed
-     *
-     * @return void
-     * @throws Exception
-     */
-    private function getHeaders(): array
-    {
-        if (!isset($this->config) && !isset($this->config['apiKey'])) {
-            throw new Exception('No apiKey was found in current config');
-        }
-
-        return [
-            'x-api-key' => $this->config['apiKey'],
-            'Content-Type' => 'application/json',
-        ];
-    }
-
-    /**
-     * Get current global data
-     *
-     * @return void
-     * @throws Exception
-     */
-    private function getData(): array
-    {
-        if (!isset($this->data)) {
-            throw new Exception('No Data found cannot build email request');
-        }
-
-        return $this->data;
+        $this->url = $url;
     }
 }
